eec36b175ee29e5558e72608e44a558c72e79ce3
RuanUFSC <ruaanlopes@hotmail.com>
2023-11-13 17:49:25 -0300

    feat: remoção de produtos

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -1,41 +1,57 @@
-import { useState, useEffect } from "react";
-import "react-toastify/dist/ReactToastify.css";
-import { Link } from "react-router-dom";
-import "./Edit.css";
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "react-toastify/dist/ReactToastify.css"
+import { Link } from "react-router-dom"
+import "./Edit.css"
+import { ToastContainer, toast } from "react-toastify"
 
 function ProdutoForm() {
-  const [produtos, setProdutos] = useState([]);
+  const [produtos, setProdutos] = useState([])
 
   useEffect(() => {
-    const fetchProdutos = async () => {
-      try {
-        const response = await fetch(
-          "https://cervejaria-backend.onrender.com/produtos/consultar-produtos"
-        );
-        const data = await response.json();
-        setProdutos(data.data);
-        console.log(data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    fetchProdutos()
+  }, [])
+  
+  const fetchProdutos = async () => {
+    try {
+      const response = await fetch(
+        "https://cervejaria-backend.onrender.com/produtos/consultar-produtos"
+      )
+      const data = await response.json()
+      setProdutos(data.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-    fetchProdutos();
-  }, []);
+  const removeProduct = async (produto) => {
+    try {
+      await axios.delete(`https://cervejaria-backend.onrender.com/produtos/apagar-produto/${produto}`)
+      toast.success("Produto excluído com sucesso!")
+      fetchProdutos()
+
+    } catch (error) {
+      toast.error("Erro ao excluir o produto. Por favor, tente novamente.")
+      console.error(error)
+    }
+  }
 
   return (
     <div>
+      <ToastContainer />  
       <h1>Editar um Produto</h1>
       {produtos &&
         produtos.map((a, index) => (
-          <>
-            <Link to={`/edit/${a.produto_id}`}>
-              <h5 key={index}>{a.nome}✏️</h5>
-            </Link>
-          </>
+          <div className="product-item" key={index}>
+            <span>{a.nome}</span>
+            <div className="buttons">
+              <Link to={`/edit/${a.produto_id}`}>✏️</Link>
+              <Link onClick={() => removeProduct(a.produto_id)}>🗑️</Link>
+            </div>
+          </div>
         ))}
     </div>
-  );
+  )
 }
 
-export default ProdutoForm;
+export default ProdutoForm
